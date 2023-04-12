8100f14cfa4f62df113af96c90018527d6985f69
Dana Freitas <danarobertfreitas@gmail.com>
2023-04-12 15:43:23 -0400

    testing for animation

--- mozart.js
+++ mozart.js
@@ -1,40 +1,90 @@
-let ham = document.getElementById("hamburger");
+const ham = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+let menc = document.getElementsByClassName("menuclass")
+
+
 ham.addEventListener("click", () => {
   document.getElementById("hamburger").classList.toggle("show");
 });
+
+
 ham.addEventListener("click", () => {
-  if (document.getElementById("menu").style.display == "block") {
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("menu").style.width = "0rem";
-  } else {
-    document.getElementById("menu").style.display = "block";
-    document.getElementById("menu").style.width = "100vw";
+  if (menu.classList.contains("menuclass"))  {
+    menu.classList.remove("menuclass");
+    menu.classList.add("menuclass2");
+  } else if (menu.classList.contains("menuclass2")) {    
+    menu.classList.remove("menuclass2");
+  menu.classList.add("menuclass");
+//document.getElementsByClassName("menuclass").style.display="none";
+
+
   }
-  console.log("test");
+
 });
 
+const keyframesRule = `
+  @keyframes myAnimation {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+ham.addEventListener("click", () =>{
+  
+})
 
 
+/*
+const appear = {
+  transform: "translate(500px, 0px)",
+};
 
-let aud = new Audio("Eine-Kleine-Nachtmusik.mp3")
+const dissappear = {
+  transform: "translate(-500px, 0px)",
+};
+
+const move = {
+  duration: 500,
+  iterations: 1,
+};
+
+menu.addEventListener("click", () => {
+  if (document.getElementById("menu").style.animate == "") {
+    menu.animate(appear, move);
+  }
+
+  else if (appear = true){
+    menu.animate(dissappear, move);
+  }
+
+  else {
+    menu.animate(appear,move)
+  }
+
+});
+*/
+
+//get elementbyid.
+//onclick for loop.
+//  paragraphs[i].style.animate = "";
+
+let aud = new Audio("Eine-Kleine-Nachtmusik.mp3");
 
 aud.play();
-
 
 const music = document.getElementById("music");
 
 music.addEventListener("click", () => {
-  
-  let aud = new Audio("Eine-Kleine-Nachtmusik.mp3")
+  let aud = new Audio("Eine-Kleine-Nachtmusik.mp3");
   let audio = document.getElementById("audio");
 
   audio.style.display = "absolute";
   aud.play();
-  
-
-})
-
-
+});
 
 let options = {
   root: null,
@@ -47,16 +97,11 @@ const observer = new IntersectionObserver((entries) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("Mozartimagetest");
       observer.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove("Mozartimagetest");
     }
-
-    else{
-      entry.target.classList.remove('Mozartimagetest');
-    }
-
-
   });
 });
-
 
 window.addEventListener(
   "DOMContentLoaded",
