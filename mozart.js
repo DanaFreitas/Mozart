360220ee35273478edb83c306eeb71a2f35a36d7
Dana Freitas <danarobertfreitas@gmail.com>
2023-04-16 13:59:34 -0400

    preparing for second menu button

--- mozart.js
+++ mozart.js
@@ -1,26 +1,24 @@
 const ham = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
-let menc = document.getElementsByClassName("menuclass0")
-
+let menc = document.getElementsByClassName("menuclass0");
 
 ham.addEventListener("click", () => {
   document.getElementById("hamburger").classList.toggle("show");
 });
 
-
 ham.addEventListener("click", () => {
-  if (menu.classList.contains("menuclass0") || (menu.classList.contains("menuclass")))  {
+  if (
+    menu.classList.contains("menuclass0") ||
+    menu.classList.contains("menuclass")
+  ) {
     menu.classList.remove("menuclass0");
     menu.classList.remove("menuclass");
     menu.classList.add("menuclass2");
-  } else if (menu.classList.contains("menuclass2")) {    
+  } else if (menu.classList.contains("menuclass2")) {
     menu.classList.remove("menuclass2");
-  menu.classList.add("menuclass");
-//document.getElementsByClassName("menuclass").style.display="none";
-
-
+    menu.classList.add("menuclass");
+    //document.getElementsByClassName("menuclass").style.display="none";
   }
-
 });
 
 const keyframesRule = `
@@ -34,78 +32,33 @@ const keyframesRule = `
   }
 `;
 
-ham.addEventListener("click", () =>{
-  
-})
-
-
-let aud = new Audio("Eine-Kleine-Nachtmusik.mp3");
-
-
-aud.play();
 
 const music = document.getElementById("musicbutton");
-//let sound = document.getElementById("test");
-let test = document.getElementById("test");
-let audioele = document.getElementById("audios");
 
-
+const audioele = document.getElementById("audios");
+const audiosource = new Audio("Eine-Kleine-Nachtmusik.mp3");
 music.addEventListener("click", () => {
 
-  let audiosource = new Audio("Eine-Kleine-Nachtmusik.mp3");
-  //let sound = document.getElementById("audios");
 
-  console.log(test)
-  console.log(audioele)
+  if ((audioele.style.display == "none" || audioele.style.display == "")) {
+    audioele.style.display = "block";
+    audiosource.play();
 
 
-  if (test.style.display == "block"){
-
-    console.log("no block");
-
-    console.log(test)
-    console.log(audioele)
-
-    test.style.display = "none"
-    test.style.padding = "50px";
-    //sound.style.display = "none";
+  }
+ else if (audioele.style.display = "block") {
     audioele.style.display = "none";
-   // audiosource.play();  
-    //console.log("testing");
-    console.log("no block");
-
-    console.log(test)
-    console.log(audioele)
-
-//whatever changes go on only are effected by the initial if statement. additionally, the other styles are uniformly wiped out,
-//replaced by an elment{} not of my creation.
-//perhaps I need to wrap it
-//wrapping doesnt work. the button is doing something, but im not sure what the exaxt problem is
-//padding effects it, but even when set to block, the element just...dissappears 
-
-  } else if (test.style.display == "none")
-{    
+    audiosource.pause();
+    audiosource.currentTime = 0;
 
 
-test.style.display = "block"
-audioele.style.display = "block"
+  }  
 
-console.log("block");
-
-console.log(test)
-console.log(audioele)
-
-
-}
-console.log("    ")
-console.log("    ")
-console.log("    ")
-
-//for some reason, the condition for the if statement never thinks its false
 });
 
 
-
+//if block, else none it works fine
+//if none else block, there needs to be a second click for it to start working
 
 let options = {
   root: null,
