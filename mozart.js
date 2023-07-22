331dd8a98ba4a018c3028e2a210458ea893c45ca
Dana Freitas <danarobertfreitas@gmail.com>
2023-07-22 04:25:38 -0400

    Working on email

--- mozart.js
+++ mozart.js
@@ -1,18 +1,18 @@
  const express = require("express");
  const app = express();
+ const sendEmail = require('./utils/sendEmail.js')
  const ejs = require('ejs');
  const port = 3000;
 const path = require('path')
  const router = express.Router();
  require('dotenv').config();
+ express.urlencoded({ extended: false }) //will alow destructuring for post 
 
- const sgMail = require('@sendgrid/mail') 
+ //const sgMail = require('@sendgrid/mail') 
  
- sgMail.setApiKey(process.env.SENDGRID_API_KEY)
- 
- //port 4000 works ok if chageed. will send emil
- 
- const apiKey = process.env.SENDGRID_API_KEY;
+ //sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  
+ //const apiKey = process.env.SENDGRID_API_KEY;
  
 
  app.set('views', path.join(__dirname, 'views'))
@@ -21,6 +21,11 @@ app.set('view engine', 'ejs');
  app.get("/", function (req, res) {
   //
   res.render("index")
+});
+
+
+app.get("/sent", (req, res) => {
+  res.render("sent");
 });
 
 
@@ -34,33 +39,60 @@ app.use('/', router)
   );
 
 
-
-app.get('/', (req, res) => {
-    res.send('Successful response.');
-console.log("TELEPORT SUCCESSFUL")
-});
-
+//})
 
 
 
 //cant have 2 app.listen(port)
 
+//{
+
+// const formsubmit = document.getElementById("submit")
+// const formrecipient = document.getElementById("email")
+// const formsubject = document.getElementById("subject")
+// const formtext = document.getElementById("text")
 
 
 
-const msg = {
-  to: 'danarobertfreitas@gmail.com',
-  from: 'danarobertfreitas@gmail.com',
-  subject: 'test',
-  text: 'testing',
-  html: '<strong>testing</strong>',
-}
+//for some reason, it has to come from me
 
-sgMail  .send(msg)
-        .then(() => {
-          console.log('Email sent')
-        })
-        .catch((error) =>{
-          console.error(error)
-        })
+app.post("/sendemail", (req,res) => {
 
+
+  const {name,email,subject,text} = req.body;
+  const from = "process.env.FROM_EMAIL";
+
+  const output = 'testing!!!'
+
+  sendEmail(output)
+  res.redirect("/sent");
+  // const msg = {
+  //   to: formrecipient,
+  //   from: 'danarobertfreitas@gmail.com',
+  //   subject: formsubject,
+  //   text: formtext,
+  //   html: 'testing',
+  // }
+
+
+// sgMail  .send(msg)
+//         .then(() => {
+//           console.log('Email sent')
+//         })
+//         .catch((error) =>{
+//           console.error(error)
+//         })
+
+//       })
+
+})
+     
+
+
+
+
+
+      // // formsubmit.addEventListener("click", (event) => {
+      // //   console.log("Log!")
+      // //  window.location.href = "localhost:3000/"
+      // // })
---
+++ utils/sendEmail.js
@@ -0,0 +1,34 @@
+const sgMail = require('@sendgrid/mail');
+ 
+ 
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+ 
+const apiKey = process.env.SENDGRID_API_KEY;
+
+
+
+
+const sendEmail = (to,from, subject, text) => {
+    const msg = {
+        to,
+        from,
+        subject,
+        text
+
+    }
+
+    sgMail  .send(msg)
+        .then(() => {
+          console.log('Email sent')
+        })
+        .catch((error) =>{
+          console.error(error)
+        })
+
+      //})
+
+
+
+}
+
+module.exports = sendEmail;
